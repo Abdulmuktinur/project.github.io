804e74511355c865972f5a223250a15b96054e2b
Abdul Mukti Nurrohman <abduldw9a@gmail.com>
2020-10-28 22:12:04 +0700

    project vue js

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logos.png">
+    <HelloWorld msg="Welcome to Vin's Care"/>
   </div>
 </template>
 
@@ -23,6 +23,11 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 50px;
+}
+
+img{
+  margin : 75px 0 25px;
+  width: 300px;
 }
 </style>
